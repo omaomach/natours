149357a788884bba14da07ab5204ec51e5ea506a
omaomach <aaronjoash44@gmail.com>
2024-08-11 01:56:32 +0300

    worked on Mongoose  document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'], // validator
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -60,8 +63,26 @@ const tourSchema = new mongoose.Schema(
 );
 
 tourSchema.virtual('durationWeeks').get(function () {
+  // virtual properties cannot be used in a query as they are technically not part of the database
   return this.duration / 7; // the "this" key word points to the current document
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+// Remember /insertMany will not trigger the .save() middleware
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
