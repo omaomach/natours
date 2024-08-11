12b578a1323056cc29ed93d9cd486f8d735b9f97
omaomach <aaronjoash44@gmail.com>
2024-08-11 23:26:31 +0300

    worked on Query Middleware: pre and post

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -83,6 +87,22 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE: runs before any find query is executed
+tourSchema.pre(/^find/, function (next) {
+  // the reqular expression helps define that the middleware should only be executed, not only for 'find' but for all query commands that start with find
+  // tourSchema.pre('find', function (next) {
+  // notice the 'find' hook instead of the 'save' hook
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
