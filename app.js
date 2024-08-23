b2842b5d9b0327c8b867ab7d3e8bb34f2470bed7
omaomach <aaronjoash44@gmail.com>
2024-08-23 23:55:48 +0300

    implemented simple error handler for non existent routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -17,13 +16,15 @@ app.use(express.json()); // this (express.json) here is called middleware
 
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
-
-// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
 
 module.exports = app;
