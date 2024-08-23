539aaa1be217dd4e2c01b0a31645ad3fe0cda6c3
omaomach <aaronjoash44@gmail.com>
2024-08-23 23:41:29 +0300

    testing branch, removed unused middle ware

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ app.use(express.json()); // this (express.json) here is called middleware
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
 //   next();
