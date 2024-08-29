ef4c6c7c3fac6bd4a35b2b6e3dd99f85d4c680e9
omaomach <aaronjoash44@gmail.com>
2024-08-29 11:43:59 +0300

    worked on error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -55,6 +61,9 @@ module.exports = (err, req, res, next) => {
     }
 
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
